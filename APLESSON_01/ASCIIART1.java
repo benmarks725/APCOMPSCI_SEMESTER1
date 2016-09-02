03bfccec52e086046a1e956303252fe3fcb74ec2
benmarks725 <benmarks725@gmail.com>
2016-09-02 11:19:16 -0700

    Added Files

---
+++ APLESSON_01/ASCIIART1.java
@@ -0,0 +1,15 @@
+public class ASCIIART1
+{
+	public static void main(String[]args)
+	{
+		System.out.println("\t $$$$$$$\\  \t              \t             ");
+		System.out.println("\t $$  __$$\\ \t              \t             ");
+		System.out.println("\t $$ |  $$ | \t    $$$$$$\\  \t   $$$$$$$\\ ");
+		System.out.println("\t $$$$$$$\\| \t   $$  __$$\\ \t   $$  __$$\\");
+		System.out.println("\t $$  __$$\\ \t   $$$$$$$$ | \t   $$ |  $$ |");
+		System.out.println("\t $$ |  $$ | \t   $$   ____| \t   $$ |  $$ |");
+		System.out.println("\t $$$$$$$  | \t \\ $$$$$$$   \t \\$$ |  $$ |");
+		System.out.println("\t \\_______/ \t  \\_______|  \t  \\__| \\__|");
+		System.out.println("\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t");
+	}
+} 
---
+++ APLESSON_01/ASCIIART2.java
@@ -0,0 +1,12 @@
+public class ASCIIART2
+{
+	public static void main(String[]args)
+	{
+		System.out.println("  _   _  _____  _____  __ ");
+		System.out.println(" | | | | _   _| _   _|  ___|");
+		System.out.println(" | |_| |  | |    | |  | |__  ");
+		System.out.println(" |  _  |   | |    | |  |  __| ");
+		System.out.println(" | | | |   | |    | |  ||___ ");
+		System.out.println(" \\| |_/    \\/    \\/   \\___/ ");
+	}
+}
