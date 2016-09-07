92418a1906ceac867506588a8e02c3f29130385e
benmarks725 <benmarks725@gmail.com>
2016-09-07 11:48:11 -0700

    Added Lab

---
+++ APLESSON_02/Lab_02.java
@@ -0,0 +1,27 @@
+public class Lab_02
+{
+	public static void main(String[]args)
+	{
+		int l = 8;
+		int w = 5;
+		int product = l * w;
+		
+		System.out.println("8 multiplied by 5 is" +" "+ product);
+		
+		String name = "Ben Marks";
+		String address = "4127 Federman Lane";
+		String number = "San Diego, CA 92130";
+		
+		System.out.println(name + "\n" + address + "\n" + number);
+		
+		
+		l = 272;
+		w = 594;
+		int h = 777;
+		double answer = 2*(w*l + h*l + h*w);
+			
+		System.out.println("The surface area of the rectangular prism is" +" "+ answer);
+		
+		
+	}
+}
