99ab973c150ff021ef21ec566765b74047841451
benmarks725 <benmarks725@gmail.com>
2016-10-13 12:08:45 -0700

    Added Labs

---
+++ APLESSON_05/Exercise_05.java
@@ -0,0 +1,64 @@
+import java.util.Scanner;
+
+public class Exercise_05
+{
+	public static void main(String[]args)
+	{
+		Scanner input = new Scanner(System.in);
+		String Go = "";
+		String Look = "";
+		String Pantry = "";
+		String Eat = "";
+
+		System.out.println(" WELCOME TO MY TINY ADVENTURE");
+		System.out.println("  ");
+		System.out.println(" You are in a creepy house! Would you like to go 'upstairs' or into the 'kitchen'? ");
+		System.out.print("> ");
+		Go = input.next();
+
+		if (Go.equalsIgnoreCase("kitchen")) 
+		{
+		System.out.println("There is a long countertop with dirty dishes everywhere. Off to one side there is, as you'd expect, a refrigerator. You may open the 'refrigerator' or look in the 'pantry'. ");
+		System.out.print(">  ");
+		Look = input.next();
+		}
+		
+		if (Look.equalsIgnoreCase("refrigerator")) 
+		{
+        System.out.println("Inside the refrigerator you see food and stuff. It looks pretty nasty. Would you like to eat some of the food, 'Yes' or 'No'?");
+        System.out.print(">  ");
+        Eat = input.next();
+		}
+		
+        if (Eat.equalsIgnoreCase("Yes")) 
+		{
+            System.out.println("  ");
+            System.out.println("You live!");
+		}
+		
+		else if (Eat.equalsIgnoreCase("No")) 
+		{
+            System.out.println("  ");
+            System.out.println("You die of starvation!");
+        }
+		
+		else if (Look.equalsIgnoreCase("pantry")) 
+		{
+        System.out.println("There is a killer inside. Do you want to 'fight' them, or 'run away'?");
+        System.out.print(">  ");
+        Pantry = input.next();
+		}
+		
+        if (Pantry.equalsIgnoreCase("fight")) 
+		{
+            System.out.println("  ");
+            System.out.println("You're weak and die");
+        }
+		
+		else if (Pantry.equalsIgnoreCase("run away")) 
+		{
+            System.out.println("  ");
+            System.out.println("You died because your too slow & can't run");
+		}
+}
+}
